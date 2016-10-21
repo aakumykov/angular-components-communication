ec643ca33e5e31c4d909e42b3d626dc93696351b
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-21 16:57:24 +0700

    Панель управляема, но архитектура запутанная

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var app = angular.module('MainApp',[
 	'PageHeader',
 	'PageFooter',
-	'ButtonsArea',
 	'SideBar',
 ]);
 
@@ -11,7 +10,7 @@ app.controller('RootController', function(){
 	this.page = {
 		title: 'Главная страница',
 		credits: 'Андрей Кумыков, 2016',
-		navigation: false,
+		sideBar: false,
 	}
 
 	this.pageTitle = function(arg=undefined){
@@ -24,12 +23,20 @@ app.controller('RootController', function(){
 	this.pageCredits = function(){
 		return this.page.credits;
 	}
-	this.navBar = function(cmd='status'){
+	this.sideBar = function(cmd){
+		console.log("RootController.sideBar('"+cmd+"')");
 		switch (cmd) {
-			case 'status':
-				return this.page.navigation;
+			//case '?' || 'status':
+			case '?':
+				return this.page.sideBar;
+			case 'show':
+				this.page.sideBar = true;
+				break;
+			case 'hide':
+				this.page.sideBar = false;
+				break;
 			case 'toggle':
-				this.page.navigation = !this.page.navigation;
+				this.page.sideBar = !this.page.sideBar;
 				break;
 			default:
 				console.log("неизвестная команда '"+cmd+"'");
--- side-bar/side-bar.component.js
+++ side-bar/side-bar.component.js
@@ -1,9 +1,34 @@
 'use strict';
 
-angular.module('SideBar').
-component('sideBar',{
+var module = angular.module('SideBar');
+
+module.component('sideBar',{
 	templateUrl: 'side-bar/side-bar.template.html',
+	controller: function(){
+		this.hideSideBar = function() {
+			console.log('hideSideBar()');
+			this.callback({
+				$event: {cmd:'hide'}
+			});
+		}
+	},
 	bindings: {
 		visible: '<',
+		callback: '&',
+	},
+});
+
+module.component('sideBarToggle',{
+	templateUrl: 'side-bar/side-bar-toggle.template.html',
+	controller: function(){
+		this.toggleSideBar = function(){
+			console.log('toggleSideBar()');
+			this.callback({
+				$event: {cmd:'toggle'}
+			});
+		}
+	},
+	bindings: {
+		callback: '&',
 	},
 });
