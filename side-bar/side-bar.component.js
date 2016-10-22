f97fbd2dc3596359fa42f5dc6b1845f357871465
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-22 23:08:15 +0700

    Улучшил смысл переменных

--- side-bar/side-bar.component.js
+++ side-bar/side-bar.component.js
@@ -5,14 +5,14 @@ var module = angular.module('SideBar');
 module.component('sideBar',{
 	templateUrl: 'side-bar/side-bar.template.html',
 	bindings: {
-		visible: '<',
-		callback: '&',
+		visibility: '<',
+		action: '&',
 	},
 });
 
 module.component('sideBarToggle',{
 	templateUrl: 'side-bar/side-bar-toggle.template.html',
 	bindings: {
-		callback: '&',
+		action: '&',
 	},
 });
